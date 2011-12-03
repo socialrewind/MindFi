122814352594ced10b42eecc25e84efa1d039cab
Bguardia <bguardia@socialrewind.com>
2011-12-03 06:30:02 +0000

    Fixes to setup and web site - Intermedia

--- MindFi/MindFi/MBBeta2.root/MBBeta2/MBSite/FBlogin_success.aspx.cs
+++ MindFi/MindFi/MBBeta2.root/MBBeta2/MBSite/FBlogin_success.aspx.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 namespace MBSite
 {
@@ -23,6 +24,29 @@ namespace MBSite
                 {
                     //this.lblResult.Text += userSNID;
                     // TODO: Save
+                    try
+                    {
+                        System.Configuration.Configuration rootWebConfig = System.Web.Configuration.WebConfigurationManager.OpenWebConfiguration("/socialrewind");
+                        System.Configuration.ConnectionStringSettings connString;
+                        if (0 < rootWebConfig.ConnectionStrings.ConnectionStrings.Count)
+                        {
+                            connString = rootWebConfig.ConnectionStrings.ConnectionStrings["SocialRewind"];
+
+                            using (SqlConnection conn = new SqlConnection(connString.ConnectionString))
+                            {
+                                conn.Open();
+                                SqlCommand insCmd = new SqlCommand("insert into Logins (SNID) values (@SNID)", conn);
+                                insCmd.Parameters.Add(new SqlParameter("SNID", userSNID));
+                                insCmd.ExecuteNonQuery();
+                                conn.Close();
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        // ignore for now
+                        Response.Write(ex.ToString());
+                    }
                 }
             }
         }

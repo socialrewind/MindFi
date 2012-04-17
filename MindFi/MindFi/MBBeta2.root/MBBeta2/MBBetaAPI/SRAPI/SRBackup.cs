6e1d9f1df09abf46c05d327c6f404ddf55a2a0ce
Bguardia <bguardia@socialrewind.com>
2012-04-17 04:54:19 +0000

    Added API for backup

---
+++ MindFi/MindFi/MBBeta2.root/MBBeta2/MBBetaAPI/SRAPI/SRBackup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MBBetaAPI.SRAPI
+{
+    public class SRBackup
+    {
+        /// <summary>
+        /// Initial date that is selected for backup
+        /// </summary>
+        public DateTime BackupPeriodSelectedStartDate { get; set; }
+        /// <summary>
+        /// End date that is selected for backup
+        /// </summary>
+        public DateTime BackupPeriodSelectedEndDate { get; set; }
+        /// <summary>
+        /// Initial date that is completed for backup
+        /// </summary>
+        public DateTime BackupCompletedStart { get; set; }
+        /// <summary>
+        /// End date date that is completed for backup
+        /// </summary>
+        public DateTime BackupCompletedEnd { get; set; }
+        /// <summary>
+        /// Initial date for the period in progress
+        /// </summary>
+        public DateTime CurrentPeriodStart { get; set; }
+        /// <summary>
+        /// End date for the period in progress
+        /// </summary>
+        public DateTime CurrentPeriodEnd { get; set; }
+
+    }
+}
